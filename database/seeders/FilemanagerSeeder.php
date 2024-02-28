cf7f25f23190f9909223f332511abefd2c19a452
fazrilrama <muhamadfathuladitya15@gmail.com>
2024-02-28 15:43:14 +0700

    Add Seeder Izin AIO

--- database/seeders/FilemanagerSeeder.php
+++ database/seeders/FilemanagerSeeder.php
@@ -31,5 +31,31 @@ class FilemanagerSeeder extends Seeder
             'slug'      =>'signature',
             'keterangan' => 'Tanda Tangan Rasya Bayu Pamungkas',
         ]);
+
+        Filemanager::create([
+            'filename' => 'B0483TTD.svg',
+            'path'     => 'assets/img/signature/B0483TTD.svg',
+            'extension'  => 'svg',
+            'id_karyawan' => 'B0483',
+            'slug'      =>'signature',
+            'keterangan' => 'Tanda Tangan Achmad Siswanto',
+        ]);
+
+        Filemanager::create([
+            'filename' => 'PFI00123TTD.svg',
+            'path'     => 'assets/img/signature/PFI00123TTD.svg',
+            'extension'  => 'svg',
+            'id_karyawan' => 'PFI00123TTD',
+            'slug'      =>'signature',
+            'keterangan' => 'Tanda Tangan REHAN ERLANGGA',
+        ]);
+        Filemanager::create([
+            'filename' => 'AIO_5202402281458.pdf',
+            'path'     => '/filemanager/file_izin/AIO_5202402281458.pdf',
+            'extension'  => 'pdf',
+            'id_karyawan' => 'B0483',
+            'slug'      =>'izin',
+            'keterangan' => 'FILE IZIN Achmad Siswanto',
+        ]);
     }
 }
--- database/seeders/IzinSeeder.php
+++ database/seeders/IzinSeeder.php
@@ -24,6 +24,7 @@ class IzinSeeder extends Seeder
             'ttd_karyawan'      => 'assets/img/signature/22001TTD.svg',
             'kembali'           => 0,
             'id_client'         => 2,
+            'status'            => 0,
             'detail'            => "TESS",
             'jam_keluar'        => '14:00'
         ]);
@@ -38,8 +39,26 @@ class IzinSeeder extends Seeder
             'ttd_karyawan'      => 'assets/img/signature/22001TTD.svg',
             'kembali'           => 1,
             'id_client'         => 2,
+            'status'            => 0,
             'detail'            => "TESS 22",
             'jam_keluar'        => '13:00'
+        ]);
+
+        Izin::create([
+            'karyawan_id'       => 'B0483',
+            'nama_karyawan'     => 'Achmad Siswanto',
+            'divisi'            => 'GA',
+            'jabatan'           => 'Pakcing',
+            'alasan'            => 'Operasional',
+            'tanggal_pembuatan' => '2024-02-02',
+            'ttd_karyawan'      => 'assets/img/signature/B0483TTD.svg',
+            'ttd_mengetahui'    => 'assets/img/signature/PFI00123TTD.svg',
+            'id_client'         => 3,
+            'alasan'            => "TESTING 1",
+            'status'            => 2,
+            'id_filemanager'    => 5,
+            'user_id_mengetahui'=> 'REHAN ERLANGGA',
+            'jam_keluar'        => '14:00'
         ]);
     }
 }
